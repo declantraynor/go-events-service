3d466fb6c489d8db99fd0af8aee88add63da41de
Declan Traynor <coderdex@gmail.com>
2015-02-15 17:46:43 +0000

    implement event storage to redis

--- interfaces/datastore.go
+++ interfaces/datastore.go
@@ -10,11 +10,29 @@ import (
 )
 
 type RedisEventStore struct {
-	conn *redis.Conn
+	conn redis.Conn
 }
 
-func (repo *RedisEventStore) Put(event domain.Event) error {
-	fmt.Println("RedisEventStore->Put")
+func (store *RedisEventStore) getEventKey() (string, error) {
+	id, err := redis.Int(store.conn.Do("INCR", "next_event_id"))
+	if err != nil {
+		return "", errors.New("Unable to generate event ID")
+	}
+	return fmt.Sprintf("event:%d", id), nil
+}
+
+func (store *RedisEventStore) Put(event domain.Event) error {
+
+	key, keyError := store.getEventKey()
+	if keyError != nil {
+		return keyError
+	}
+
+	_, err := store.conn.Do("HMSET", key, "name", event.Name, "timestamp", event.Timestamp)
+	if err != nil {
+		return errors.New("Unable to store event")
+	}
+
 	return nil
 }
 
@@ -24,5 +42,5 @@ func NewRedisEventStore(addr, port string) (RedisEventStore, error) {
 	if err != nil {
 		return RedisEventStore{}, errors.New("Unable to connect to redis")
 	}
-	return RedisEventStore{conn: &conn}, nil
+	return RedisEventStore{conn: conn}, nil
 }
--- interfaces/datastore_test.go
+++ interfaces/datastore_test.go
@@ -1,10 +1,14 @@
 package interfaces
 
 import (
+	"fmt"
 	"log"
 	"testing"
 
+	"github.com/garyburd/redigo/redis"
 	"github.com/stvp/tempredis"
+
+	"github.com/declantraynor/go-events-service/domain"
 )
 
 func startRedis(port string) *tempredis.Server {
@@ -28,10 +32,11 @@ func stopRedis(server *tempredis.Server) {
 
 func TestNewRedisEventStore(t *testing.T) {
 	server := startRedis("12313")
+	defer stopRedis(server)
+
 	if _, err := NewRedisEventStore("127.0.0.1", "12313"); err != nil {
 		t.Fail()
 	}
-	stopRedis(server)
 }
 
 func TestNewRedisEventStoreUnableToConnect(t *testing.T) {
@@ -39,5 +44,56 @@ func TestNewRedisEventStoreUnableToConnect(t *testing.T) {
 	_, err := NewRedisEventStore("127.0.0.1", "6379")
 	if err == nil || err.Error() != expectedError {
 		t.Errorf("expected error %q, got %q", expectedError, err.Error())
+	}
+}
+
+func TestGetEventKeyFormat(t *testing.T) {
+	server := startRedis("12313")
+	defer stopRedis(server)
+
+	store, _ := NewRedisEventStore("127.0.0.1", "12313")
+
+	expected := "event:1"
+	if key, _ := store.getEventKey(); key != expected {
+		t.Errorf("expected event key %q, got %q", expected, key)
+	}
+}
+
+func TestGetEventKeyIncrementsByOne(t *testing.T) {
+	server := startRedis("12313")
+	defer stopRedis(server)
+
+	store, _ := NewRedisEventStore("127.0.0.1", "12313")
+
+	var key string
+	for i := 1; i < 11; i++ {
+		expected := fmt.Sprintf("event:%d", i)
+		key, _ = store.getEventKey()
+		if key != expected {
+			t.Errorf("expected key %q, got %q", expected, key)
+		}
+	}
+}
+
+func TestPutEvent(t *testing.T) {
+	server := startRedis("12313")
+	defer stopRedis(server)
+
+	store, _ := NewRedisEventStore("127.0.0.1", "12313")
+	event := domain.Event{Name: "test", Timestamp: "2015-02-11T15:01:00+00:00"}
+
+	if err := store.Put(event); err != nil {
+		t.Error(err)
+	}
+
+	conn, _ := redis.Dial("tcp", "127.0.0.1:12313")
+	defer conn.Close()
+
+	name, err := redis.String(conn.Do("HGET", "event:1", "name"))
+	if err != nil {
+		t.Error(err)
+	}
+	if name != event.Name {
+		t.Error(err)
 	}
 }
