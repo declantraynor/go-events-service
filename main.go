5958673dba7b1508e5be4b2ba5ee2e840f1e3aef
Declan Traynor <coderdex@gmail.com>
2015-02-18 13:09:36 +0000

    refactor main for easier testing

--- main.go
+++ main.go
@@ -10,8 +10,7 @@ import (
 	"github.com/declantraynor/go-events-service/usecases"
 )
 
-func main() {
-
+func run(serve func(webservice *web.WebService)) {
 	redisAddr := os.Getenv("REDIS_PORT_6379_TCP_ADDR")
 	redisPort := os.Getenv("REDIS_PORT_6379_TCP_PORT")
 	eventStore, err := datastore.NewRedisEventStore(redisAddr, redisPort)
@@ -22,7 +21,15 @@ func main() {
 	eventInteractor := usecases.EventInteractor{Store: &eventStore}
 	webservice := web.WebService{EventInteractor: &eventInteractor}
 
+	serve(&webservice)
+}
+
+func serve(webservice *web.WebService) {
 	http.HandleFunc("/events", webservice.Create)
 	http.HandleFunc("/events/count", webservice.Count)
 	http.ListenAndServe(":5000", nil)
+}
+
+func main() {
+	run(serve)
 }
